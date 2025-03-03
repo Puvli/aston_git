93ce158b93e0818d4a826b37255e4e8f01d95ec9
Puvli44 <pavel-zajtsev@yandex.ru>
2025-03-03 19:14:16 +0300

    feature/homework-2

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("Привет, мир!");
+function myPrompt() {
+    let number = prompt("Введите число");
+    
+    if (!Number.isInteger(Number(number))) {
+        alert("Некорректный ввод!");
+        return;
+    }
+
+    let base = prompt("Введите систему счисления для числа");
+    
+    if (!Number.isInteger(Number(base)) || base < 2 || base > 36) {
+        alert("Некорректный ввод!");
+        return;
+    }
+
+    let convertedNumber = Number(number).toString(Number(base));
+    
+    alert(`Ответ: число ${number} в ${base}-ой системе счисления = ${convertedNumber}`);
+}
+
+myPrompt();
