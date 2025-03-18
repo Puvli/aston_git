80078732f62b76a7f017aa40056c6fb311a50408
Puvli44 <pavel-zajtsev@yandex.ru>
2025-03-18 21:47:24 +0300

    hw-7

--- app.js
+++ app.js
@@ -1,21 +1,55 @@
-function myPrompt() {
-    let number = prompt("Введите число");
-    
-    if (!Number.isInteger(Number(number))) {
-        alert("Некорректный ввод!");
-        return;
+//1.расширение функции
+function memoize(callback) {
+  const cache = {};
+
+  return function (...args) {
+    const key = JSON.stringify([...args].sort()); // Сортируем аргументы, чтобы порядок не влиял на ключ
+    console.log(key);
+    console.log("cache before ", cache);
+
+    if (cache[key] !== undefined) {
+      console.log("Get from cache");
+      return cache[key];
     }
 
-    let base = prompt("Введите систему счисления для числа");
-    
-    if (!Number.isInteger(Number(base)) || base < 2 || base > 36) {
-        alert("Некорректный ввод!");
-        return;
-    }
+    console.log("First calculation");
+    const result = callback(...args);
+    cache[key] = result;
+    console.log("cache after ", cache);
 
-    let convertedNumber = Number(number).toString(Number(base));
-    
-    alert(`Ответ: число ${number} в ${base}-ой системе счисления = ${convertedNumber}`);
+    return result;
+  };
 }
 
-myPrompt();
+//2. каррированная функция
+function add(a) {
+  return function (b) {
+    if (b !== undefined) {
+      return add(a + b); //рекурсивно вызываем add(a + b), накапливая сумму
+    }
+    return a;
+  };
+}
+
+//3. контекст
+function logger() {
+  console.log(`I output only external context: ${this.item}`);
+}
+
+const obj = { item: "some value" };
+
+const boundLogger = logger.bind(obj);
+boundLogger();
+
+console.log("call");
+logger.call(obj);
+
+console.log("apply");
+logger.apply(obj);
+
+//4. Полифил
+function myBind(fn, context, ...boundArgs) {
+  return function (...args) {
+    return fn.apply(context, [...boundArgs, ...args]);
+  };
+}
